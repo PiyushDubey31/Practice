160e9f61e6e60020c53b1c2e169e38e124042dc9
PiyushDubey31 <piyushnit77@gmail.com>
2020-12-10 17:03:50 +0530

    code of props

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,44 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import User from './user'
+import UserCard from '../component/usercard'
 
-export default function Home() {
+
+export default function Home(props) {
+  const card=[
+    {
+      img: '/images/img1.jpg',
+      name:'flower'
+    },
+    {
+      img: '/images/img2.jpg',
+      name:'hii'
+    },
+    {
+      img: '/images/img4.jpg',
+      name:'jii'
+    },
+    {
+      img: 'https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg',
+      name:'hlw'
+    },
+
+  ]
   return (
+    
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+     <h1>welcome piyush dubey to new project</h1>
+     {/* <User name="piyush dubey" age="23" /> */}
+     {
+       card.map((item,index)=>(
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+         <UserCard image={item.img} name={item.name} key={index}/>  
+       ))
+     }
+     
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+  
+      
     </div>
   )
 }
---
+++ pages/user.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function User(props) {
+    const name=null;
+     const age=null;
+  console.log(props.name,props.age);
+    return (
+        <div>
+            <h1>hii this is props</h1>
+            
+       
+        </div>
+    );
+}
+
+export default User;
